a1fd534294a151cc45507c6d5a6ed76168b45733
SadheeraMahanama <sadheeramahanama94@gmail.com>
2019-09-21 16:19:00 +0530

    Changed name of delete button

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -135,12 +135,12 @@ class Counter extends Component {
         >
           Increment Button
         </button>
-        {/* <button
+        <button
           onClick={() => this.props.onDelete(this.props.counter.id)}
           className="btn btn-danger btn-sm m-2"
         >
-          Delete
-        </button> */}
+          Delete Button
+        </button>
       </div>
     );
   }
